6d304704e1848618c18083db3f714761588ce637
Sammy Gaskins <70036190+scgaskins@users.noreply.github.com>
2020-09-27 14:20:13 -0500

    Board can check if a list of positions spells a word

--- lib/Game/Game.dart
+++ lib/Game/Game.dart
@@ -5,15 +5,46 @@ import 'package:boggle/Position.dart';
 
 class Game {
   GameBoard _board;
-  int _size;
+  int _boardSize;
   Set _validWords;
   Set _submittedWords;
 
-  Game(this._size, int randomSeed) {
-    _board = GameBoard(_size, Random(randomSeed));
+  Game(this._boardSize, int randomSeed) {
+    _board = GameBoard(_boardSize, Random(randomSeed));
     _submittedWords = Set();
     _validWords = Set();
   }
 
   String getLetterAtPosition(Position pos) => _board.getLetterAtPosition(pos.column, pos.row);
+
+  bool _validPosition(Position pos) => pos.row >= 0 && pos.row < _boardSize && pos.column >= 0 && pos.column < _boardSize;
+
+  bool isWordValid(List<Position> positions) {
+    if (!_wordPositionsValid(positions)) {
+      return false;
+    }
+    String wordString = _convertPositionListToString(positions);
+    if (_validWords.contains(wordString)) {
+      _submittedWords.add(wordString);
+      return true;
+    }
+    return false;
+  }
+
+  bool _wordPositionsValid(List<Position> positions) {
+    for (int i=1; i<positions.length; i++) {
+      if (!_validPosition(positions[i]) || !_validPosition(positions[i-1]) || !positions[i].isNeighbor(positions[i-1])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  String _convertPositionListToString(List<Position> positions) {
+    String wordString = '';
+    for (Position pos in positions) {
+      wordString += getLetterAtPosition(pos).toUpperCase();
+    }
+    return wordString;
+  }
 }
--- lib/Position.dart
+++ lib/Position.dart
@@ -3,4 +3,31 @@ class Position{
   int column;
 
   Position(this.column, this.row);
+
+  bool isNeighbor(Position other) {
+    return directlyAbove(other) || directlyBelow(other) || directlyLeft(other) || directlyRight(other) || inDiagonal(other);
+  }
+
+  bool directlyAbove(Position other) {
+    return column == other.column && row == other.row - 1;
+  }
+
+  bool directlyBelow(Position other) {
+    return column == other.column && row == other.row + 1;
+  }
+
+  bool directlyLeft(Position other) {
+    return row == other.row && column == other.column - 1;
+  }
+
+  bool directlyRight(Position other) {
+    return row == other.row && column == other.column + 1;
+  }
+
+  bool inDiagonal(Position other) {
+    if (row == other.row + 1 || row == other.row - 1) {
+      return column == other.column + 1 || column == other.column - 1;
+    }
+    return false;
+  }
 }
